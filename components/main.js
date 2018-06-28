283848f45372e357e54f1701067b06f25becb9a2
Todd James <todd@itodd.co>
2018-06-28 07:24:21 -0400

    Code working to post to express (code is ugly)

--- components/main.js
+++ components/main.js
@@ -1,3 +1,5 @@
+
+
 const InputForm = {
     template: ` 
     <div class="columns">
@@ -103,6 +105,106 @@ const InputForm = {
                                 </label>
                                 <label class="radio">
                                     <input type="radio" name="modALine1" value="16" v-model="modA1Q"> &nbsp;16
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="field is-horizontal">
+                    <div class="field-label">
+                        <select v-model="modA2">
+                        <option value="000">--select--</option>
+                        <option value="SPACE1">Space 1 RU</option>
+                        <option value="SPACE2">Space 2 RU</option>
+                        <option value="SPACE6">Space 6 RU</option>
+                        <option value="PANEL1">Panel 1 RU</option>
+                        <option value="PANEL2">Panel 2 RU</option>
+                        <option value="PANEL6">Panel 6 RU</option>
+                        <option value="PATCH1">Patch 1 RU</option>
+                        <option value="PANDUIT1">Panduit 1 RU</option>
+                        <option value="PANDUIT2">Panduit 2 RU</option>
+                        <option value="PANDUIT3">Panduit 3 RU</option>
+                        <option value="PANDUCT1">Duct 1 RU</option>
+                        <option value="PANDUCT2">Duct 2 RU</option>
+                        <option value="PANRING1">D - Rings 1 RU</option>
+                        <option value="PANTRAY1">Tray 1 RU</option>
+                        <option value="PDU208M1">PDU RP208 M1</option>
+                        <option value="PDU208M3">PDU RP208 M3</option>
+                        <option value="000">-- Nexus --</option>
+                        <option value="NX5548P">Nexus 5548P</option>
+                        <option value="NX5548UP">Nexus 5548UP</option>
+                        <option value="NX5596P">Nexus 5596P</option>
+                        <option value="NX5596UP">Nexus 5596UP</option>
+                        <option value="000">-- Cisco MDS --</option>
+                        <option value="MDS9124">MDS 9124</option>
+                        <option value="MDS9132T">MDS 9132T</option>
+                        <option value="MDS9134">MDS 9134</option>
+                        <option value="MDS9148S">MDS 9148S</option>
+                        <option value="MDS9148">MDS 9148</option>
+                        <option value="MDS9221I">MDS 9221i</option>
+                        <option value="MDS9222I">MDS 9222i</option>
+                        <option value="MDS9250I">MDS 9250i</option>
+                        <option value="MDS9396S">MDS 9396S</option>
+                        <option value="MDS9506">MDS 9506</option>
+                        <option value="MDS9509">MDS 9509</option>
+                        <option value="MDS9513">MDS 9513</option>
+                        <option value="MDS9706">MDS 9706</option>
+                        <option value="MDS9710">MDS 9710</option>
+                        <option value="MDS9718">MDS 9718</option>
+                        <option value="000">-- NetApp --</option>
+                        <option value="NA2020">NetApp FAS 2020</option>
+                        <option value="NA2240">NetApp FAS 2240</option>
+                        <option value="NA2520">NetApp FAS 2520</option>
+                        <option value="NA2552">NetApp FAS 2552</option>
+                        <option value="NA2554">NetApp FAS 2554</option>
+                        <option value="NA2600">NetApp FAS 2600</option>
+                        <option value="NA3210">NetApp FAS 3210</option>
+                        <option value="NA6210">NetApp FAS 6210</option>
+                        <option value="NA8020">NetApp FAS 8020</option>
+                        <option value="NA8040">NetApp FAS 8040</option>
+                        <option value="NA8060">NetApp FAS 8060</option>
+                        <option value="NA8080">NetApp FAS 8080</option>
+                        <option value="NA8200">NetApp FAS 8200</option>
+                        <option value="NA9000">NetApp FAS 9000</option>
+                        <option value="NA540">NetApp EF540</option>
+                        <option value="NA200">NetApp AFF A200</option>
+                        <option value="NA300">NetApp AFF A300</option>
+                        <option value="NA700">NetApp AFF A700</option>
+                        <option value="NA700S">NetApp AFF A700s</option>
+                        <option value="NA2246">NetApp DS2246</option>
+                        <option value="NA224C">NetApp DS224C</option>
+                        <option value="NA4243">NetApp DS4243</option>
+                    </select>
+                    </div>
+                    <div class="field-body">
+                        <div class="field is-narrow">
+                            <div class="control has-text-centered">
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="0" v-model="modA2Q"> &nbsp;0
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="1" v-model="modA2Q"> &nbsp;1
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="2" v-model="modA2Q"> &nbsp;2
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="3" v-model="modA2Q"> &nbsp;3
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="4" v-model="modA2Q"> &nbsp;4
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="6" v-model="modA2Q"> &nbsp;6
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="8" v-model="modA2Q"> &nbsp;8
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="12" v-model="modA2Q"> &nbsp;12
+                                </label>
+                                <label class="radio">
+                                    <input type="radio" name="modALine2" value="16" v-model="modA2Q"> &nbsp;16
                                 </label>
                             </div>
                         </div>
@@ -239,21 +341,43 @@ const InputForm = {
         return {
             modA1: 000,
             modA1Q: 0,
+            modA2: 000,
+            modA2Q: 0,
             ruSelection: 42,
             rackFace: 'FRONT',
             frontBezel: 'YES',
             rackLoad: 'HIGH',
             panels: 'YES',
             paramsStart: '<params ',
-            build: ''
+            build: '',
+            rackArray: [],
+            rackHead: '<rack cfg="1" qty="1">\n',
+            indent: '   ',
+            rackFooter: '</rack>',
+            rackTag: '',
         }
     },
     methods: {
         submitForm(evt) {
             evt.preventDefault();
             this.build = '';
+            this.rackTag= '';
             this.build = this.build + this.paramsStart + 'ru="' + this.ruSelection + '" extra="0" ';
 
+            this.rackArray[0] = this.modA1;
+            this.rackArray[1] = this.modA1Q;
+            this.rackArray[2] = this.modA2;
+            this.rackArray[3] = this.modA2Q;
+
+            this.rackTag = this.rackTag + this.rackHead;
+            
+            for(var item = 0; item < this.rackArray.length; item += 2) {
+                this.rackTag = this.rackTag + this.indent + '<device model="' + this.rackArray[item] + '" qty="' + this.rackArray[(item + 1)] + '" />\n';
+            }
+
+            this.rackTag = this.rackTag + this.rackFooter + '\n';
+
+            console.log(this.rackTag);
 
             if (this.rackFace === "FRONT") {
                 this.build = this.build + 'face="FRONT" ';
@@ -285,6 +409,11 @@ const InputForm = {
 
             this.build = this.build + ('/>');
             console.log(this.build);
+
+            var url= 'http://localhost:3000/xml'
+            axios.post(url, { data: this.rackTag}).then(function(response) {
+                //console.log('response: ' + response);
+            });
         }
     }
 }
--- express.js
+++ express.js
@@ -1,6 +1,63 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => res.send('UCS Config'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: true }));
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
+var parser = require('xml2json');
+const axios = require('axios');
+var fs = require('fs');
+
+let xmlInfo = `<?xml version="1.0" encoding="UTF-8"?>
+<layout mode="MANUAL" src="UCS" seq="" res="LOW">
+    <chassis model="UCS5109" cfg="1" qty="1">
+        <power qty="4" />
+    </chassis>
+    <rack cfg="1" qty="1">
+        <device model="SPACE2" qty="1" />
+        <device model="PANDUIT2" qty="1" />
+        <device model="PANDUCT2" qty="1" />
+        <device model="PANTRAY1" qty="1" />
+    </rack>
+    <params ru="60" extra="0" face="PERSPECTIVE" bezel="NO" fill="LINEAR" load="HIGH" panels="YES" logo="CISCO" format="PHOTO" size="LARGE" trans="NO" />
+</layout>`;
+
+// Post xml string to usc4 server
+var url = "http://ucs4.us/ucs/engine/ucs-engine.cgi";
+xhr.open("POST", url, false);
+xhr.setRequestHeader("Content-Type", "text/xml");
+
+// test xml to json
+var json = parser.toJson(xmlInfo);
+
+// convert json to xml
+var xml = parser.toXml(json);
+
+
+
+app.get('/', (req, res, next) => {
+    console.log(xml);
+});
+
+app.post('/xml', (req, res, next) => {
+    res.end(JSON.stringify(req.body));
+    
+    
+    var conv1 = parser.toJson(req.body.data);
+    console.log('\n\nData recieved from website (JSON): ' + conv1);
+    var conv2 = parser.toXml(conv1);
+    console.log('\n\nData converted to XML in Node (XML): ' + conv2);
+    
+});
 
 app.listen(3000, () => console.log('UCS Config listening on port 3000'));
